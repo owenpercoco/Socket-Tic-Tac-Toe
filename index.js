dafe7f84e6ded4b03b5e76a484c20c4fd136d5b5
owenpercoco <owenpercoco@gmail.com>
2017-06-09 11:09:52 -0400

    port adjustment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 // Run server to listen on port 3000.
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
 });
 
